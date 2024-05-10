7279594d741fba7bde2f47179a4dd17f6d052930
Kim de Bie <kimdebie@outlook.com>
2024-05-10 16:37:38 +0200

    improve utils

--- scraper/utils.py
+++ scraper/utils.py
@@ -8,16 +8,15 @@ def get_author_id(author: str) -> int:
     return AUTHORS.index(author)
 
 
-def replace_authors(text: str, no_space: bool = False) -> str:
+def replace_authors(text: str) -> str:
     """Replace name of authors/accounts in text"""
     for author in AUTHORS:
         text = text.replace(f"@{author}", f"@{get_author_id(author)}")
         text = text.replace(f"{author} ", f"@{get_author_id(author)} ")
-        if no_space:
-            text = text.replace(author, f"@{get_author_id(author)}")
+        text = text.replace(author, f"@{get_author_id(author)}")
     return text
 
 
 def clean(text: str) -> str:
     """Simple cleaning of strings"""
-    return text.strip().replace("\n", " ")
+    return " ".join(text.split())
