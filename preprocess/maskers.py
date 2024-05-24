fa7a099590971c773443b5f8d00d677cb818a08b
Kim de Bie <kimdebie@outlook.com>
2024-05-24 10:38:26 +0200

    some edits

--- preprocess/maskers.py
+++ preprocess/maskers.py
@@ -5,7 +5,7 @@ from preprocess import Preprocessor
 
 class AccountMasker(Preprocessor):
     def __init__(self):
-        self.regex = r'(@\d+$)'
+        self.regex = r'@\d+'
 
     def __call__(self, sentence: str) -> str:
         return " ".join("USER" if re.match(self.regex, word) else word for word in sentence.split())
